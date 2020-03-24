9a83db0e377ed653a390298c147f021fe5b2138f
Andrii Nevidomyi <andrii.nevidomyi@ubisoft.com>
2020-03-24 22:58:48 +0200

    Task 1 string interpolation

--- Program.cs
+++ Program.cs
@@ -9,11 +9,41 @@ namespace HomeworkTemplate
         {
             Func<Task1, string> TaskSolver = task =>
             {
-                // Your solution goes here
-                // You can get all needed inputs from task.[Property]
-                // Good luck!
+                //Andrii Nevidomyi solution
 
-                return "My answer";
+                //Example
+                //[task.City.Name] = "Virtualiev"
+                //[task.City.Population] = 1400
+                //[task.City.SickPercentage] = 0.3
+                //[task.Virus.Name] = "Hlomanda"
+                //[task.Virus.KillProbability] = 0.2
+
+                //"There are 420 people sick with Hlomanda in the city of Virtualiev, 84 of which died"
+
+                //------------------------------------------------------------------------------------
+                //parse
+                float population, sickPersentage, killProbability;
+
+                //result
+                float sickPeople = 0, peopleDied = 0;
+
+                try
+                {
+                    population = Convert.ToSingle(task.City.Population);
+                    sickPersentage = Convert.ToSingle(task.City.SickPercentage);
+                    killProbability = Convert.ToSingle(task.Virus.KillProbability);
+
+                    sickPeople = population * sickPersentage;
+                    peopleDied = sickPeople * killProbability;
+
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("FormatException");
+                }
+
+
+                return $"There are {Math.Truncate(sickPeople)} people sick with {task.Virus.Name} in the city of {task.City.Name}, {Math.Truncate(peopleDied)} of which died";
             };
 
             Task1.CheckSolver(TaskSolver);
